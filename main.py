4a7f09442a30ec33e3bcee82b37b4cf30ad1ea8f
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2022-09-16 15:55:00 -0700

    Refactored for organization

--- main.py
+++ main.py
@@ -22,37 +22,49 @@ pygame.display.set_caption("Minesweeper")
 
 # Import sprites
 cell_sprites = {
-  "1": pygame.image.load("sprites/cell_1.png"),
-  "2": pygame.image.load("sprites/cell_2.png"),
-  "3": pygame.image.load("sprites/cell_3.png"),
-  "4": pygame.image.load("sprites/cell_4.png"),
-  "5": pygame.image.load("sprites/cell_5.png"),
-  "6": pygame.image.load("sprites/cell_6.png"),
-  "7": pygame.image.load("sprites/cell_7.png"),
-  "8": pygame.image.load("sprites/cell_8.png"),
-  "default": pygame.image.load("sprites/cell_default.png"),
-  "empty": pygame.image.load("sprites/cell_empty.png"),
-  "flag": pygame.image.load("sprites/cell_flag.png"),
-  "mine_clicked": pygame.image.load("sprites/cell_mine_clicked.png"),
-  "mine_false": pygame.image.load("sprites/cell_mine_false.png"),
-  "mine": pygame.image.load("sprites/cell_mine.png")
+  "1": pygame.image.load("sprites/cell/1.png"),
+  "2": pygame.image.load("sprites/cell/2.png"),
+  "3": pygame.image.load("sprites/cell/3.png"),
+  "4": pygame.image.load("sprites/cell/4.png"),
+  "5": pygame.image.load("sprites/cell/5.png"),
+  "6": pygame.image.load("sprites/cell/6.png"),
+  "7": pygame.image.load("sprites/cell/7.png"),
+  "8": pygame.image.load("sprites/cell/8.png"),
+  "default": pygame.image.load("sprites/cell/default.png"),
+  "empty": pygame.image.load("sprites/cell/empty.png"),
+  "flag": pygame.image.load("sprites/cell/flag.png"),
+  "mine_clicked": pygame.image.load("sprites/cell/mine_clicked.png"),
+  "mine_false": pygame.image.load("sprites/cell/mine_false.png"),
+  "mine": pygame.image.load("sprites/cell/mine.png")
 }
 
 frame_sprites = {
-  "top_left": pygame.image.load("sprites/frame_top_left.png"),
-  "top_right": pygame.image.load("sprites/frame_top_right.png"),
-  "left": pygame.image.load("sprites/frame_left.png"),
-  "right": pygame.image.load("sprites/frame_right.png"),
-  "bottom": pygame.image.load("sprites/frame_bottom.png"),
-  "bottom_left": pygame.image.load("sprites/frame_bottom_left.png"),
-  "bottom_right": pygame.image.load("sprites/frame_bottom_right.png")
+  "top_left": pygame.image.load("sprites/frame/top_left.png"),
+  "top_right": pygame.image.load("sprites/frame/top_right.png"),
+  "left": pygame.image.load("sprites/frame/left.png"),
+  "right": pygame.image.load("sprites/frame/right.png"),
+  "bottom": pygame.image.load("sprites/frame/bottom.png"),
+  "bottom_left": pygame.image.load("sprites/frame/bottom_left.png"),
+  "bottom_right": pygame.image.load("sprites/frame/bottom_right.png")
 }
 
 face_sprites = {
-  "default": pygame.image.load("sprites/face_default.png"),
-  "pressed": pygame.image.load("sprites/face_pressed.png"),
-  "waiting": pygame.image.load("sprites/face_waiting.png"),
-  "sad": pygame.image.load("sprites/face_sad.png")
+  "default": pygame.image.load("sprites/face/default.png"),
+  "pressed": pygame.image.load("sprites/face/pressed.png"),
+  "waiting": pygame.image.load("sprites/face/waiting.png"),
+  "sad": pygame.image.load("sprites/face/sad.png")
+}
+
+counter_sprites = {
+  "1": pygame.image.load("sprites/counter/1.png"),
+  "2": pygame.image.load("sprites/counter/2.png"),
+  "3": pygame.image.load("sprites/counter/3.png"),
+  "4": pygame.image.load("sprites/counter/4.png"),
+  "5": pygame.image.load("sprites/counter/5.png"),
+  "6": pygame.image.load("sprites/counter/6.png"),
+  "7": pygame.image.load("sprites/counter/7.png"),
+  "8": pygame.image.load("sprites/counter/8.png"),
+  "9": pygame.image.load("sprites/counter/9.png")
 }
 
 # Game state variables
